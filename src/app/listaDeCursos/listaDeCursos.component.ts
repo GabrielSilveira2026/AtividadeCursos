b3e41c527770726e54fb84e9af319e904e3dcb12
gabriel.silveira8 <gabriel.silveira20@bol.com.br>
2021-08-18 22:20:38 -0300

    Atividade Lista de Cursos

---
+++ src/app/listaDeCursos/listaDeCursos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listaDeCursos',
+  templateUrl: './listaDeCursos.component.html',
+  styleUrls: ['./listaDeCursos.component.css']
+})
+export class ListaDeCursosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
